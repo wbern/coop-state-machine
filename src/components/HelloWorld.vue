9680f87388e2ad992804d5a3d53f2cc0c8efeaad
William Bernting <william.bernting@kits.se>
2019-10-02 21:46:13 +0200

    almost

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,17 +6,23 @@
                 :key="gridX"
                 class="grid-block"
                 :class="{
-                    'grid-block--valid': validSpace(gridX, gridY),
-                    'grid-block--in-use': validSpace(gridX, gridY) && inUse(gridToCoords(gridX, gridY))
+                    'grid-block--valid-floor-space': validFloorSpace(gridX, gridY),
+                    'grid-block--valid-stack-space': validStackSpace(gridX, gridY),
+                    'grid-block--floor-space-in-use': validFloorSpace(gridX, gridY) && floorSpaceInUse(gridToCoords(gridX, gridY)),
+                    'grid-block--stack-space-in-use': validStackSpace(gridX, gridY) && stackSpaceInUse(gridX, gridY)
                 }"
             >
-                <div v-if="inUse(gridToCoords(gridX, gridY))">
-                    <img
-                        v-if="validSpace(gridX, gridY)"
-                        :style="'opacity: 1; z-index: ' + getZ(gridX, gridY) + ';'"
-                        class="grid-image"
-                        :src="getCoordsData(gridToCoords(gridX, gridY))[0].type"
-                    />
+                <div
+                    v-if="validFloorSpace(gridX, gridY) && floorSpaceInUse(gridToCoords(gridX, gridY))"
+                    :style="'opacity: 1; z-index: ' + getFloorSpaceZ(gridX, gridY) + ';'"
+                >
+                    <!-- <img class="grid-image" :src="getCoordsData(gridToCoords(gridX, gridY)).type" /> -->
+                </div>
+                <div
+                    v-if="validStackSpace(gridX, gridY) && stackSpaceInUse(gridX, gridY)"
+                    :style="'opacity: 1; z-index: ' + getStackSpaceZ(gridX, gridY) + ';'"
+                >
+                    <!-- <img class="grid-image" :src="findStackSpaceCoords(gridX, gridY).type" /> -->
                 </div>
             </div>
         </div>
@@ -34,10 +40,33 @@ export default {
             // return y.toString().padStart(3, '0')
         },
         gridToCoords(gridX, gridY) {
-            return {
-                x: gridX / 2 - ((gridX / 2) % 2),
-                y: gridY / 2 - ((gridY / 2) % 2),
+            if (gridX === 1 && gridY === 1) {
+                debugger
             }
+
+            if (!this.validFloorSpace(gridX, gridY)) {
+                throw new Error(
+                    'cannot convert to coords with uneven grid coords'
+                )
+            }
+
+            // return {
+            //     x: gridX / 2,
+            //     y: gridY / 2,
+            // }
+
+            let offset = 0
+
+            if (this.isOddSpace(gridX, gridY)) {
+                offset = 1
+            }
+
+            const res = {
+                x: gridX / 2,
+                y: (gridY - offset) / 2,
+            }
+
+            return res
         },
         isEvenSpace(gridX, gridY) {
             return gridX % 2 === 0 && gridY % 2 === 0
@@ -45,42 +74,76 @@ export default {
         isOddSpace(gridX, gridY) {
             return gridX % 2 === 1 && gridY % 2 === 1
         },
-        validSpace(gridX, gridY) {
+        validFloorSpace(gridX, gridY) {
             return (
                 this.isEvenSpace(gridX, gridY) || this.isOddSpace(gridX, gridY)
             )
         },
-        inUse(coords) {
+        validStackSpace(gridX, gridY) {
+            // anywhere is valid stack space
+            return true
+            // return (
+            //     !this.isEvenSpace(gridX, gridY) &&
+            //     !this.isOddSpace(gridX, gridY)
+            // )
+        },
+        floorSpaceInUse(coords) {
             return !!this.getCoordsData(coords)
+        },
+        stackSpaceInUse(gridX, gridY) {
+            return this.findStackSpaceCoords(gridX, gridY)
+        },
+        findStackSpaceCoords(startingGridX, startingGridY) {
+            for (let i = startingGridY + 1; i < this.rows * 2; i++) {
+                if (this.validFloorSpace(startingGridX, i)) {
+                    let coords = this.gridToCoords(startingGridX, i)
+
+                    let res = this.getCoordsData({
+                        x: coords.x,
+                        y: coords.y,
+                        z: i - startingGridY,
+                    })
+
+                    if (
+                        // * are there any floors this many rows to the right?
+                        // * is floor in use?
+                        // * does the floor have high enough elevation?
+                        res
+                    ) {
+                        return res
+                    }
+                }
+            }
         },
         getCoordsData(coords) {
             return (
-                this.coordsData[coords.x] && this.coordsData[coords.x][coords.y]
+                this.coordsData[coords.x] &&
+                this.coordsData[coords.x][coords.y] &&
+                this.coordsData[coords.x][coords.y][coords.z || 0]
             )
         },
-        getZ(x, y) {
-            let isStacked = false
-
-            if (isStacked) {
-                return this.rows - y
-            } else {
-                return 'inherit'
-            }
+        getFloorSpaceZ(x, y) {
+            return y
+        },
+        getStackSpaceZ(x, y) {
+            return y + 2
         },
     },
     data() {
         // y
-        let coordsData = new Array(this.columns).fill(
+        let coordsData = new Array(this.columns).fill(1).map(y =>
             // x
-            new Array(this.rows).fill(
-                // z
-                new Array(this.initialFloors).fill(
-                    { type: 'buildings/buildingTiles_000.png' },
-                    0,
-                    this.rows
-                )
+            new Array(this.rows).fill(1).map(x =>
+                // z (floors)
+                new Array(this.initialFloors).fill(1).map(z => ({
+                    type: 'buildings/buildingTiles_000.png',
+                }))
             )
         )
+
+        coordsData[1][0] = new Array(3).fill({
+            type: 'buildings/buildingTiles_000.png',
+        })
 
         return {
             coordsData,
@@ -95,6 +158,10 @@ export default {
         columns: {
             type: Number,
             default: () => 6,
+        },
+        initialFloors: {
+            type: Number,
+            default: () => 0,
         },
     },
 }
@@ -128,11 +195,17 @@ export default {
     /* for stacking buildings */
     /* height: 10px; */
 }
-.grid-block--valid {
-    background: rgba(0, 100, 255, 0.1);
+.grid-block--valid-stack-space {
+    background: rgba(189, 66, 255, 0.1);
 }
-.grid-block--in-use {
-    background: rgba(2, 255, 50, 0.5);
+.grid-block--valid-floor-space {
+    background: rgba(0, 255, 50, 0.1);
+}
+.grid-block--floor-space-in-use {
+    background: rgba(0, 255, 50, 1);
+}
+.grid-block--stack-space-in-use {
+    background: rgba(200, 50, 255, 1);
 }
 .grid-image {
     /* height: 4rem;
