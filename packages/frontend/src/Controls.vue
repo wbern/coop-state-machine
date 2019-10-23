df6a081f89dbdbf1f783189150d7cba613c300f8
William Bernting <william.bernting@kits.se>
2019-10-23 13:13:21 +0200

    super nice visual turn thing

--- packages/frontend/src/Controls.vue
+++ packages/frontend/src/Controls.vue
@@ -1,79 +1,97 @@
 <template>
     <div class="city-controls">
-        <ui-icon-button
-            @click="onSkipToBack"
-            :disabled="!canUndo || currentTurn === 0"
-            type="secondary"
-            class="city-controls__icon"
-            icon="restore"
-        ></ui-icon-button>
-        <ui-modal ref="rewindModal" title="Rewind back to turn..">
-            <ui-textbox
-                label="Turn #"
-                type="number"
-                :min="0"
-                :max="currentTurn - 1"
-                v-model.number="rewindToTurnNumber"
-            ></ui-textbox>
-            <ui-button
-                raised
-                class="city-controls__icon city-controls__icon--mirror"
-                type="primary"
+        <div class="left">
+            <small>Turn #{{ currentTurn }}</small>
+        </div>
+        <div class="center">
+            <ui-icon-button
+                @click="onSkipToBack"
+                :disabled="!canUndo || currentTurn === 0"
+                type="secondary"
+                class="city-controls__icon"
                 icon="restore"
-                :disabled="!(rewindToTurnNumber >= 0 && rewindToTurnNumber <= 1000 && rewindToTurnNumber < currentTurn)"
-                :icon-position="'left'"
-                :size="'normal'"
-                @click="onRewindToTurnRequest"
-                >Rewind to this turn
-            </ui-button>
-        </ui-modal>
-        <ui-icon-button
-            @click="onBack"
-            :disabled="!canUndo || currentTurn === 0"
-            type="secondary"
-            class="city-controls__icon"
-            icon="skip_previous"
-        ></ui-icon-button>
-        <ui-icon-button
-            @click="onPlay"
-            :disabled="true"
-            type="primary"
-            class="city-controls__icon"
-            icon="play_arrow"
-        ></ui-icon-button>
-        <ui-icon-button
-            @click="onForward"
-            :disabled="!(canRedo || canTick)"
-            type="secondary"
-            class="city-controls__icon"
-            icon="skip_next"
-        ></ui-icon-button>
-        <ui-icon-button
-            @click="onSkipToEnd"
-            type="secondary"
-            class="city-controls__icon city-controls__icon--mirror"
-            icon="restore"
-        ></ui-icon-button>
-        <ui-modal ref="forwardModal" title="Skip to turn..">
-            <ui-textbox
-                label="Turn #"
-                type="number"
-                :min="currentTurn + 1"
-                :max="1000"
-                v-model.number="forwardToTurnNumber"
-            ></ui-textbox>
-            <ui-button
-                raised
+            ></ui-icon-button>
+            <ui-modal ref="rewindModal" title="Rewind back to turn..">
+                <ui-textbox
+                    label="Turn #"
+                    type="number"
+                    :min="0"
+                    :max="currentTurn - 1"
+                    v-model.number="rewindToTurnNumber"
+                ></ui-textbox>
+                <ui-button
+                    raised
+                    class="city-controls__icon city-controls__icon--mirror"
+                    type="primary"
+                    icon="restore"
+                    :disabled="
+                        !(
+                            rewindToTurnNumber >= 0 &&
+                            rewindToTurnNumber <= 1000 &&
+                            rewindToTurnNumber < currentTurn
+                        )
+                    "
+                    :icon-position="'left'"
+                    :size="'normal'"
+                    @click="onRewindToTurnRequest"
+                    >Rewind to this turn
+                </ui-button>
+            </ui-modal>
+            <ui-icon-button
+                @click="onBack"
+                :disabled="!canUndo || currentTurn === 0"
+                type="secondary"
+                class="city-controls__icon"
+                icon="skip_previous"
+            ></ui-icon-button>
+            <ui-icon-button
+                @click="onPlay"
+                :disabled="true"
+                type="primary"
+                class="city-controls__icon"
+                icon="play_arrow"
+            ></ui-icon-button>
+            <ui-icon-button
+                @click="onForward"
+                :disabled="!(canRedo || canTick)"
+                type="secondary"
+                class="city-controls__icon"
+                icon="skip_next"
+            ></ui-icon-button>
+            <ui-icon-button
+                @click="onSkipToEnd"
+                type="secondary"
                 class="city-controls__icon city-controls__icon--mirror"
                 icon="restore"
-                type="primary"
-                :disabled="!(forwardToTurnNumber > 0 && forwardToTurnNumber <= 1000 && forwardToTurnNumber > currentTurn)"
-                :icon-position="'left'"
-                :size="'normal'"
-                @click="onForwardToTurnRequest"
-                >Skip to this turn
-            </ui-button>
-        </ui-modal>
+            ></ui-icon-button>
+            <ui-modal ref="forwardModal" title="Skip to turn..">
+                <ui-textbox
+                    label="Turn #"
+                    type="number"
+                    :min="currentTurn + 1"
+                    :max="1000"
+                    v-model.number="forwardToTurnNumber"
+                ></ui-textbox>
+                <ui-button
+                    raised
+                    class="city-controls__icon city-controls__icon--mirror"
+                    icon="restore"
+                    type="primary"
+                    :disabled="
+                        !(
+                            forwardToTurnNumber > 0 &&
+                            forwardToTurnNumber <= 1000 &&
+                            forwardToTurnNumber > currentTurn
+                        )
+                    "
+                    :icon-position="'left'"
+                    :size="'normal'"
+                    @click="onForwardToTurnRequest"
+                    >Skip to this turn
+                </ui-button>
+            </ui-modal>
+        </div>
+        <div class="right"></div>
     </div>
 </template>
 <script>
@@ -92,11 +110,11 @@ export default {
     methods: {
         onRewindToTurnRequest() {
             this.$emit('tick-to-turn-request', this.rewindToTurnNumber)
-            this.$refs['rewindModal'].close();
+            this.$refs['rewindModal'].close()
         },
         onForwardToTurnRequest() {
             this.$emit('tick-to-turn-request', this.forwardToTurnNumber)
-            this.$refs['forwardModal'].close();
+            this.$refs['forwardModal'].close()
         },
         onSkipToBack() {
             this.$refs['rewindModal'].open()
@@ -124,13 +142,20 @@ export default {
 }
 </style>
 <style scoped>
+.left,
+.right {
+
+    flex: 1;
+}
 .city-controls__icon {
     margin: 0 4px;
 }
 
 .city-controls {
     display: flex;
+    align-items: center;
     justify-content: center;
     margin: 4px 0;
+    padding: 0 10px;
 }
 </style>
